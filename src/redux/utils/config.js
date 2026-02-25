89660f8117e18a1f156e59f01b1a15808af13ce7
Farhan Ragrez <120573311+Farhanrangrez-dev@users.noreply.github.com>
2026-02-25 14:26:05 +0530

    Update BASE_URL to production environment

--- src/redux/utils/config.js
+++ src/redux/utils/config.js
@@ -2,5 +2,6 @@
 // const BASE_URL= "https://sarnic-backend-production-690c.up.railway.app/api"; 
 
 // localhost link
-const BASE_URL= "http://localhost:9000/api"; 
+// const BASE_URL= "http://localhost:9000/api"; 
+const BASE_URL= "https://saaranik-backend-full-ok-code-production.up.railway.app/api"; 
 export const apiUrl = BASE_URL;
